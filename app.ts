93d92605a0577c191d6f1d4b6bafd0c3d39f4ba8
aslam_php <aslam@webandcrafts.com>
2024-06-24 15:07:08 +0530

    PORT BUG FIXED

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import { publishToService2 } from "./app/services/service";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 4000;
+const port = process.env.APP_PORT || 4000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Publisher server");
